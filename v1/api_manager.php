2f5c90615264cfe9da7d5cc0f6eb85213f387c8a
Christopher <christopher.oneisz@icloud.com>
2024-10-20 16:09:55 +0200

    Added hotfix for crashing pluginData with invalid JSON
    #4 put the post check into index.php instead of api manager

--- v1/api_manager.php
+++ v1/api_manager.php
@@ -1,14 +1,13 @@
 <?php
 
-/// import of database manager and class
+// Import of database manager and class
 include('db_manager.php');
 
-/// this class is creating some data and profil infos in the database
-
+// This class is creating some data and profile infos in the database
 class api_manager
 {
 
-    /// this function gets all the data from the database like player profile
+    // This function gets all the data from the database like player profile
     public function api_get_playerpprofil($uuid)
     {
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
@@ -23,39 +22,38 @@ class api_manager
         }
     }
 
-        /// this function delets all the data form a specific player profile
-        public function api_delete_playerpprofil($uuid)
-        {
-            if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-    
-                $sql_action = new db_manager();
-                $sql_action->database_delete_playerpprofil($uuid);
-            } else {
-                http_response_code(404);
-                header('Content-Type: application/json');
-                echo json_encode(['error' => 'Needs to be a DELETE']);
-                exit;
-            }
-        }
+    // This function deletes all the data for a specific player profile
+    public function api_delete_playerpprofil($uuid)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
 
-    /// creates the profile for the player connecting the first time
+            $sql_action = new db_manager();
+            $sql_action->database_delete_playerpprofil($uuid);
+        } else {
+            http_response_code(404);
+            header('Content-Type: application/json');
+            echo json_encode(['error' => 'Needs to be a DELETE']);
+            exit;
+        }
+    }
+
+    // Creates the profile for the player connecting the first time
     public function api_create_playerprofil()
     {
         $sql_action = new db_manager();
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $data = json_decode(file_get_contents("php://input"), true);
 
-            $data = json_decode(file_get_contents("php://input"), true);
-
-            // scope selction from json file
-            $section_value = $data['section'];
+        if ($data !== null) {
+            // Scope selection from JSON file
+            $section_value = $data['section'] ?? '';
             $basic_value_uuid = $data['schema']['uuid'] ?? null;
             $basic_value_name = $data['schema']['name'] ?? null;
 
-            // pattern for schema in database
+            // Pattern for schema in database
             $basic_value_data = '{
                 "basicData": {
-                    "version":"v1",
+                    "version": "v1",
                     "uuid": "' . $basic_value_uuid . '",
                     "name": "' . $basic_value_name . '"
                 },
@@ -64,93 +62,80 @@ class api_manager
                 },
                 "pluginData": {
                     "values": {}
-                    }
-                }';
+                }
+            }';
 
-            /// convert to a real json string
+            // Convert to a real JSON string
             $minified_json = json_encode(json_decode($basic_value_data, true));
 
-            /// get all from basic data current only uuid (there is no changing value)
+            // Get all from basic data, currently only UUID (there is no changing value)
             if ($section_value === 'newPlayerData' && !empty($basic_value_uuid) && !empty($basic_value_name)) {
                 $sql_action->database_create_playerprofil($basic_value_uuid, $basic_value_name, $minified_json);
             }
         }
     }
 
-    /// modifyes name in Base Profile - First seciton
+    // Modifies name in base profile - First section
     public function api_update_base()
     {
         $sql_action = new db_manager();
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $data = json_decode(file_get_contents("php://input"), true);
 
-            $data = json_decode(file_get_contents("php://input"), true);
+        if ($data !== null) {
+            // Select scope
+            $section_value = $data['section'] ?? '';
 
-            /// select scope
+            // Get all from basic data, currently only UUID (there is no changing value)
+            if ($section_value === 'basicData') {
+                $basic_value_uuid = $data['schema']['uuid'] ?? null;
+                $basic_value_name = $data['schema']['name'] ?? null;
 
-            $section_value = $data['section'];
-
-            /// get all from basic data current only uuid (there is no changing value)
-            if ($section_value == 'basicData') {
-                $basic_value_uuid = $data['schema']['uuid'];
-                $basic_value_name = $data['schema']['name'];
-
-                $sql_action->database_update_base($basic_value_uuid, $basic_value_name);
+                if ($basic_value_uuid && $basic_value_name) {
+                    $sql_action->database_update_base($basic_value_uuid, $basic_value_name);
+                }
             }
         }
     }
 
-    /// modifyes all the static values in advancedData - Second section
+    // Modifies all the static values in advancedData - Second section
     public function api_update_advancedData()
     {
         $sql_action = new db_manager();
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $data = json_decode(file_get_contents("php://input"), true);
 
-            $data = json_decode(file_get_contents("php://input"), true);
+        if ($data !== null && $data['section'] === 'advancedData') {
+            $advanced_value_uuid = $data['schema']['uuid'] ?? null;
+            $language = $data['schema']['language'] ?? null;
 
-            /// get all from basic data current only uuid (there is no changing value)
-            if ($data['section'] == 'advancedData') {
-                $advanced_value_uuid = $data['schema']['uuid'];
-
-                $language = $data['schema']['language'];
-
-
-                /// hand over to database 
+            if ($advanced_value_uuid && $language) {
+                // Hand over to database
                 $sql_action->database_update_advancedData($advanced_value_uuid, $language);
             }
         }
     }
 
-    /// modifyes all the static values in pluginData - Third section
+    // Modifies all the static values in pluginData - Third section
     public function api_update_pluginData()
     {
-        $sql_action = new db_manager();
+        // Read JSON data from the POST body
+        $data = json_decode(file_get_contents("php://input"), true);
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Check if the JSON data was parsed correctly
+        if ($data !== null && isset($data['schema'])) {
+            $sql_action = new db_manager();
 
-            // Read JSON data from the POST body
-            $data = json_decode(file_get_contents("php://input"), true);
+            // Check if both 'uuid' and 'updater' exist in the 'schema'
+            if (isset($data['schema']['uuid']) && isset($data['schema']['updater'])) {
+                $uuid = $data['schema']['uuid'];
+                $json = $data['schema']['updater'];
 
-            // Check if the JSON data was parsed correctly
-            if (json_last_error() === JSON_ERROR_NONE) {
-
-                // Check if the section is 'pluginData'
-                if ($data['section'] == 'pluginData') {
-
-                    // Check if both 'uuid' and 'updater' exist in the 'schema'
-                    if (isset($data['schema']['uuid']) && isset($data['schema']['updater'])) {
-
-                        $uuid = $data['schema']['uuid'];
-                        $json = $data['schema']['updater'];
-
-                        $sql_action->database_update_pluginData($uuid, $json);
-                    } else {
-                        http_response_code(404);
-                        header('Content-Type: application/json');
-                        echo json_encode(['error' => 'There is something missing in the schema']);
-                    }
-                }
+                $sql_action->database_update_pluginData($uuid, $json);
+            } else {
+                http_response_code(404);
+                header('Content-Type: application/json');
+                echo json_encode(['error' => 'There is something missing in the schema']);
             }
         }
     }
--- v1/index.php
+++ v1/index.php
@@ -11,36 +11,56 @@ $auth_action = new auth();
 
 
 // Authenticate the API key
-if ($auth_action->bearer_token_auth() == true){
+if ($auth_action->bearer_token_auth() == true) {
 
     // API actions are executed only if authentication is successful
     $api_action = new api_manager();
 
     // Get user profile
-    if(isset($_GET['uuid'])){
+    if (isset($_GET['uuid'])) {
         $api_action->api_get_playerpprofil($_GET['uuid']);
     }
 
     // get key to delete profile
-    if(isset($_GET['delete'])){
+    if (isset($_GET['delete'])) {
         $api_action->api_delete_playerpprofil($_GET['delete']);
     }
 
-    // Create user
-    $api_action->api_create_playerprofil();
+    $data_schema = json_decode(file_get_contents("php://input"), true);
 
-    // Update base
-    $api_action->api_update_base();
+    if (is_array($data_schema)) {
+        if (isset($data_schema['section'])) {
 
-    // Update advanced data
-    $api_action->api_update_advancedData();
-
-    // Update plugin data
-    $api_action->api_update_pluginData();
-
+            /// check if there is a new profile needs to be created
+            if ($data_schema['section'] == 'newPlayerData') {
+                $api_action->api_create_playerprofil();
+            }
+    
+            /// check if it is a basic data manipulation
+            if ($data_schema['section'] == 'basicData') {
+                $api_action->api_update_base();
+            }
+    
+            /// check if advanced Data needs to be modified
+            if ($data_schema['section'] == 'advancedData') {
+                $api_action->api_update_advancedData();
+            }
+    
+            /// check if plugin data needs to be added
+            if ($data_schema['section'] == 'pluginData') {
+                $api_action->api_update_pluginData();
+            }
+        } else {
+            // Fehler: 'section' wurde nicht im JSON angegeben
+            http_response_code(400);
+            echo json_encode(['error' => 'Missing section in request data']);
+        }
+    } else {
+        // Fehler: Eingehende Daten sind kein gültiges JSON
+        http_response_code(400);
+        echo json_encode(['error' => 'Invalid JSON format']);
+    }
 } else {
     // Authentication failed, the error message is already sent in the auth class
     exit;
 }
-
-?>
